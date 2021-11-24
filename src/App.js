bea4b0305e4ded81e1a0477ada9426d7aff593af
Igor Marchenko <igor.ms83@gmail.com>
2021-11-24 20:58:30 +0300

    add project fieles for gh-pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,65 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import {CardList} from './components/card-list/card-list.component';
+import {SearchBox} from './components/search-box/search-box.component';
+
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends Component {
+
+  constructor() {
+    super()
+
+    this.state ={
+      monsters: [],
+      searchField : ""
+    }
+    // this.handleChange = this.handleChange.bind(this)
+    this.handle2 = this.handle1.bind(this) 
+  }
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => {
+      console.log(users)
+      this.setState({monsters:users}
+        )
+      })
+  }
+
+  handleChange = (e) => {
+    this.setState({searchField : e.target.value})
+  }
+
+  handle1() {
+    console.log('1 clicked')
+  }
+  
+  handle3 = () => {
+    console.log('3 clicked')
+  }
+
+  render () {
+    const {monsters, searchField } = this.state
+    const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLocaleLowerCase()))
+
   return (
     <div className="App">
+      
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+       <h1 className="rolodex">Mohsters Rolodex</h1> 
+      <SearchBox
+      placeholder='search monster'
+      handleChange={this.handleChange}
+      />  
+      <CardList monsters={filteredMonsters} />
+      
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+
+        
+
+        {/* <p>
+          Edit <code>src/App.js</code> and save to reload!!!!
         </p>
         <a
           className="App-link"
@@ -16,10 +68,12 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
+      
     </div>
   );
+  }
 }
 
 export default App;
